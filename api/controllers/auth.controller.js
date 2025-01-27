8c023fcb75480cd681abc77156d91e71413dbb3f
Arman-for-dev <armanfordev59@gmail.com>
2025-01-27 21:43:40 +0600

    complete signin ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -28,7 +28,7 @@ export const signIn = async (req, res, next) =>{
     const {email, password} = req.body;
 
     try {
-        const validUser = await User.findOne({email});
+        const validUser = await User.findOne({ email });
         if(!validUser) return next(errorHandler(404, 'User not found'));
 
         const validPassword = bcrypt.compareSync(password, validUser.password);
