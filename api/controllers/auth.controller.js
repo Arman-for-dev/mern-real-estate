3bd6d78c25c579f33caafc38921eecc389d12de4
Arman-for-dev <armanfordev59@gmail.com>
2025-01-27 19:11:14 +0600

    create sign api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-
+import jwt from 'jsonwebtoken';
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -20,5 +20,26 @@ export const signup =  async (req, res, next)=>{
         res.status(201).json('User created successfully')
     } catch (error) {
         next(errorHandler(550, 'Internal Error'))
+    }
+}
+
+
+export const signIn = async (req, res, next) =>{
+    const {email, password} = req.body;
+
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+
+        const validPassword = bcrypt.compareSync(password, validUser.password);
+        if(!validPassword) return next(errorHandler(404, 'Password does not match!'));
+
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWTSECRET, {expiresIn: '7d'});
+
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+    } catch (error) {
+        next(error);
     }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signIn, signup } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 
 router.post('/sign-up', signup);
+router.post('/sign-in', signIn);
 
 export default router
